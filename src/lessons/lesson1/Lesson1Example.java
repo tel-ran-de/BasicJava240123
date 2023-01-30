27b570e3409c2e5c418d633b82aec078a2b30050
StanislavPB <perminovsb@gmail.com>
2023-01-30 13:46:48 +0200

    lesson1

--- src/lessons/lesson1/Lesson1Example.java
+++ src/lessons/lesson1/Lesson1Example.java
@@ -3,5 +3,25 @@ package lessons.lesson1;
 public class Lesson1Example {
     public static void main(String[] args) {
 
+
+        double x = -5;
+
+        double y = 3;
+
+
+        double result = x % y; // result = 5 - (y * 2) = 5 -4 = 1
+
+        // y = 3 -> 5 - 3*1 = 2
+
+        System.out.println(result);
+
+        int a =10;
+        int b = 4;
+        double result1 = a / (double) b;
+
+
+        System.out.println(result1);
+
     }
+
 }
---
+++ src/lessons/lesson1/Lesson1Example2.java
@@ -0,0 +1,11 @@
+package lessons.lesson1;
+
+public class Lesson1Example2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
