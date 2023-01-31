1bb147f0506d1608d1b5ad30022a666c3537f167
StanislavPB <perminovsb@gmail.com>
2023-01-31 12:52:27 +0200

    lesson2

---
+++ src/lessons/lesson2/Lesson2ClassPointExample.java
@@ -0,0 +1,23 @@
+package lessons.lesson2;
+
+public class Lesson2ClassPointExample {
+    public static void main(String[] args) {
+        int a = 5;
+
+        Point point = new Point();
+
+        point.xCoordinate = 1;
+        point.yCoordinate = 5;
+        point.comment = "New point 1";
+
+        Point point2 = new Point();
+        point2.xCoordinate = 10;
+        point2.yCoordinate = 25;
+        point2.comment = "Second point";
+
+
+
+
+
+    }
+}
---
+++ src/lessons/lesson2/Point.java
@@ -0,0 +1,8 @@
+package lessons.lesson2;
+
+public class Point {
+
+    String comment;
+    int xCoordinate;
+    int yCoordinate;
+}
