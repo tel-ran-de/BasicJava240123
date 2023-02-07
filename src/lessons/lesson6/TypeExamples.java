6da42a09c0485f64b75fd90594a0666eec9b2ba8
StanislavPB <perminovsb@gmail.com>
2023-02-07 12:34:44 +0200

    lesson 6 lessoncode

---
+++ src/lessons/lesson6/TypeExamples.java
@@ -0,0 +1,31 @@
+package lessons.lesson6;
+
+public class TypeExamples {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        double y = 1256254646254765.97547;
+
+        long xLong = x;
+
+        double xDouble = xLong;
+
+        long yInt = (long) y;
+
+        System.out.println(yInt);
+
+        /*
+        byte
+        short
+        int
+        long
+        float
+        double
+
+         */
+
+
+
+    }
+}
